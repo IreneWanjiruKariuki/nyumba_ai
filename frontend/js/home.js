b3cdd2720d15575723755c5fe298f2db28b6d583
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2026-09-12 14:17:46 +0300

    populating greetings on the homepage

---
+++ frontend/js/home.js
@@ -0,0 +1,14 @@
+// PROTECT PAGE
+requireAuth();
+
+// POPULATE GREETING
+const client = getClient();
+
+if (client) {
+    const firstName = client.name.split(' ')[0];
+    const greeting  = document.getElementById('heroGreeting');
+    const welcome   = document.getElementById('welcomeName');
+
+    if (greeting) greeting.textContent = `Welcome back, ${firstName}`;
+    if (welcome)  welcome.textContent  = firstName;
+}
