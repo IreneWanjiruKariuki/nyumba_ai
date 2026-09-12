5926caba9c0d7b530172671b14f70d54760c8f5f
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2026-09-12 13:50:56 +0300

    defining the management of JWT token and storing and retrieving client's data

--- frontend/js/fetch.js
+++ frontend/js/fetch.js
@@ -55,4 +55,29 @@ async function apiFetchForm(endpoint, formData, method = 'POST') {
     } catch (error) {
         throw error;
     }
+}// AUTH HELPERS
+function saveToken(token) {
+    localStorage.setItem('token', token);
+}
+
+function getToken() {
+    return localStorage.getItem('token');
+}
+
+function removeToken() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('client');
+}
+
+function saveClient(clientData) {
+    localStorage.setItem('client', JSON.stringify(clientData));
+}
+
+function getClient() {
+    const data = localStorage.getItem('client');
+    return data ? JSON.parse(data) : null;
+}
+
+function isLoggedIn() {
+    return !!localStorage.getItem('token');
 }
